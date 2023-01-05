0536f78717f3300a461e1a7dad48cd8f226a2a19
hemantb24 <hemant.b2002@gmail.com>
2023-01-06 00:57:22 +0530

    "microsoft first question leetcode solution"

---
+++ evaluateRPN.java
@@ -0,0 +1,29 @@
+class Solution {
+    private int i;
+    String[] tokens;
+
+    public int evalRPN(String[] tokens) {
+        i = tokens.length - 1;
+        this.tokens = tokens;
+        return eval();
+    }
+
+    private int eval() {
+        String val = tokens[i--];
+
+        switch (val) {
+            case "+":
+                return eval() + eval();
+            case "-":
+                return -eval() + eval();
+            case "*":
+                return eval() * eval();
+            case "/":
+                int n2 = eval();
+                int n1 = eval();
+                return n1 / n2;
+            default:
+                return Integer.parseInt(val);
+        }
+    }
+}
